75090d99865445551b52ae3f63ce48c76c78e35d
Joakim Andén <janden@flatironinstitute.org>
2018-05-15 16:19:22 -0400

    Use correct precision in `sim_noise_image`

--- matlab/simulation/sim_noise_image.m
+++ matlab/simulation/sim_noise_image.m
@@ -18,14 +18,16 @@
 %    Joakim Anden <janden@flatironinstitute.org>
 
 function im = sim_noise_image(sim, start, num)
-    im = zeros([sim.L*ones(1, 2) num]);
+    precision = class(sim.vols);
+
+    im = zeros([sim.L*ones(1, 2) num], precision);
 
     rand_push();
 
     for s = start:start+num-1
         rand_state(sim.noise_seed + 191*s);
 
-        im_s = sqrt(sim.noise_var)*randn(sim.L*ones(1, 2));
+        im_s = sqrt(sim.noise_var)*randn(sim.L*ones(1, 2), precision);
 
         im(:,:,s-start+1) = im_s;
     end
