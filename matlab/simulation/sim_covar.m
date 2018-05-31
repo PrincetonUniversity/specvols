2aaa607d767fd3e14cae858eff627d4986a5bb96
Joakim Andén <janden@flatironinstitute.org>
2018-05-31 11:41:11 -0400

    Refactor `sim_covar` to call `sim_eigs`

--- matlab/simulation/sim_covar.m
+++ matlab/simulation/sim_covar.m
@@ -11,21 +11,17 @@
 %       an L-by-L-by-L-by-L-by-L-by-L volume matrix.
 %
 % See also
-%    sim_mean
+%    sim_eigs, sim_mean
 
 % Author
 %    Joakim Anden <janden@flatironinstitute.org>
 
 function covar_true = sim_covar(sim)
-    C = size(sim.vols, 4);
+    [eigs_true, lambdas_true] = sim_eigs(sim);
 
-    vols_c = bsxfun(@minus, sim.vols, sim_mean(sim));
+    eigs_true = vol_to_vec(eigs_true);
 
-    p = ones(1, C)/C;
-
-    vols_c = vol_to_vec(vols_c);
-
-    covar_true = vols_c*diag(p)*vols_c';
+    covar_true = eigs_true*lambdas_true*eigs_true';
 
     covar_true = vecmat_to_volmat(covar_true);
 end
