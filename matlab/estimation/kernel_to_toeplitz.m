1ce846b47492deeb0af26fddc9fd83f72d09affe
Joakim Andén <janden@flatironinstitute.org>
2018-07-18 17:05:07 -0400

    Allow setting `L` in `kernel_to_toeplitz`
    This lets us use the same function to calculate circulant matrices
    as well.

--- matlab/estimation/kernel_to_toeplitz.m
+++ matlab/estimation/kernel_to_toeplitz.m
@@ -1,11 +1,12 @@
 % KERNEL_TO_TOEPLITZ Convert 3D Fourier kernel into 3D Toeplitz matrix
 %
 % Usage
-%    A = kernel_to_toeplitz(kernel_f);
+%    A = kernel_to_toeplitz(kernel_f, L);
 %
 % Input
 %    kernel_f: The Fourier transform of the convolution kernel in the form of
-%       2*L-by-2*L-by-2*L volume. The Fourier transform must be centered.
+%       an M-by-M-by-M volume. The Fourier transform must be centered.
+%    L: The size of the volumes to be convolved (default M/2).
 %
 % Output
 %    A: An six-dimensional Toeplitz matrix of size L describing the convolu-
@@ -17,8 +18,10 @@
 % Author
 %    Joakim Anden <janden@flatironinstitute.org>
 
-function A = kernel_to_toeplitz(kernel_f)
-    L = size(kernel_f, 1)/2;
+function A = kernel_to_toeplitz(kernel_f, L)
+    if nargin < 2 || isempty(L)
+        L = size(kernel_f, 1)/2;
+    end
 
     A = eye(L^3, class(kernel_f));
 
