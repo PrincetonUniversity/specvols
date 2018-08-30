4d3bfddbb2a87985f537f0e4cf7e39a1b5d4d2a5
Amit Moscovich <moscovich@gmail.com>
2018-08-30 18:57:19 -0400

    Just testing the slack/github channel.

--- matlab/graph/test_graph_knn.m
+++ matlab/graph/test_graph_knn.m
@@ -1,9 +1,5 @@
 function test_graph_knn()
-    X = [7 8;
-        5 1;
-        3 4;
-        8 7;
-        9 3];
+    X = [7 8; 5 1; 3 4; 8 7; 9 3]; 
 
     W = graph_knn(X,2);
     
@@ -13,6 +9,6 @@ function test_graph_knn()
         32 13 0 0 0;
         2 0 0 0 17;
         29 20 0 17 0];
-    
+
     assert(isequal(round(full(W).^2), EXPECTED_DISTANCES_SQUARED), "blah"); 
 end
