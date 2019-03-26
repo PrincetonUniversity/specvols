ffb15358bacee82db279f908c057968687c22af2
Amit Moscovich <moscovich@gmail.com>
2019-03-26 18:09:47 -0400

    Minor changes to experimental/mosco examples

--- matlab/experimental/mosco/draw_pca_vs_laplacian_blob1d.m
+++ matlab/experimental/mosco/draw_pca_vs_laplacian_blob1d.m
@@ -1,9 +1,9 @@
-function [laplacian_eigvecs, laplacian_basis_coefs] = draw_pca_vs_laplacian_blob1d()
+function [laplacian_eigvecs, laplacian_basis_coefs] = draw_pca_vs_laplacian_blob1d(noise)
 L = 100;
-N_BASIS_VECTORS = 25;
+N_BASIS_VECTORS = 10;
 BLOB_WIDTH = 4;
 
-data = gen_moving_blob_1d(L, BLOB_WIDTH);
+data = gen_moving_blob_1d(L, BLOB_WIDTH, noise);
 figure;
 subplot(3,3,1)
 
@@ -33,13 +33,16 @@ caxis([-0.2 0.2]);
 colorbar;
 
 subplot(3,3,6)
-imagesc(pca_basis*pca_basis_coefs);
+pca_reconstruction = pca_basis*pca_basis_coefs; 
+imagesc(pca_reconstruction);
 title('PCA reconstruction');
+fprintf('PCA reconstruction error: %f', norm(pca_reconstruction-data,'fro'));
 
-W = graph_knn(data, 5);
+W = graph_gaussian_kernel(data, 0.1);
 laplacian_matrix = laplacian(W, 'normalized');
-[laplacian_eigvecs,D] = eigs(laplacian_matrix,N_BASIS_VECTORS,'smallestabs'); 
-assert(all(size(laplacian_eigvecs) == [L N_BASIS_VECTORS]));
+[laplacian_eigvecs,D] = eigs(laplacian_matrix,N_BASIS_VECTORS+1,'smallestabs');
+assert(all(size(laplacian_eigvecs) == [L N_BASIS_VECTORS+1]));
+laplacian_eigvecs = laplacian_eigvecs(:,2:end); % Get rid of DC
 subplot(3,3,7);
 imagesc(laplacian_eigvecs')
 title('Laplacian Eigenvectors')
@@ -55,7 +58,9 @@ caxis([-0.2 0.2]);
 colorbar;
 
 subplot(3,3,9)
-imagesc(laplacian_eigvecs*laplacian_basis_coefs);
+laplacian_reconstruction = laplacian_eigvecs*laplacian_basis_coefs;
+imagesc(laplacian_reconstruction);
 title('Laplacian reconstruction');
-end
+fprintf('Laplacian reconstruction error: %f', norm(laplacian_reconstruction-data,'fro'));
 
+end
--- matlab/experimental/mosco/gen_moving_blob_1d.m
+++ matlab/experimental/mosco/gen_moving_blob_1d.m
@@ -1,9 +1,10 @@
-function outmat = gen_moving_blob_1d(L,sigma)
-    blob = fspecial('gaussian', [1 L], sigma);
+function outmat = gen_moving_blob_1d(L,blobwidth, noise_sigma)
+    blob = fspecial('gaussian', [1 L], blobwidth);
     outmat = zeros([L L]);
     for i=1:L
         outmat(i,:) = circshift(blob, i-1);
     end
-    outmat = outmat + normrnd(0,0.01,[L L]);
+    outmat = outmat + normrnd(0,noise_sigma,[L L]);
+    outmat = outmat - sum(outmat,1)/L;
 end
 
