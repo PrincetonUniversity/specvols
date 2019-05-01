59c5c3b52e6e633894d0edc3351a04ee2b385887
Amit Moscovich <moscovich@gmail.com>
2019-05-01 13:39:52 -0400

    Set diagonal of graph gaussian kernel weights matrix to zero. This improves the robustness to noise. [1]
    [1] El Karoui, Noureddine Wu, Hau Tieng "Graph connection Laplacian methods
    can be made robust to noise" (2012)

--- matlab/graph/graph_gaussian_kernel.m
+++ matlab/graph/graph_gaussian_kernel.m
@@ -11,4 +11,7 @@
 function W = graph_gaussian_kernel(X, sigma)
     pairwise_distances = squareform(pdist(X));
     W = exp(-pairwise_distances.^2 / (2*sigma^2));
+    for i=1:length(W)
+        W(i,i) = 0;
+    end
 end
